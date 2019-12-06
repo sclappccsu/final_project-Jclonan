f08476a0f34f68f3315ac83c89f1ee8e3d92277f
profduality <professor.duality@gmail.com>
2019-12-06 17:25:36 -0500

    First commit of Twitter author file

---
+++ auth.py
@@ -0,0 +1,4 @@
+consumer_key        = '8USXTDdkoRod16LaM7SgYy0kV'
+consumer_secret     = 'cIAT465xGI10HrYZ6H7ODg2pz8TZ3IcQStAsoCEkjSDA5ZSQjZ'
+access_token        = '1202247522971832320-x1UXiwI8Nzh1r3DSEiEcKeOpRWcreV'
+access_token_secret = 'SAuG7ZP5spoCKVmuDsfr2UxdaKpK34czsD9aCITbVCDJ3'
